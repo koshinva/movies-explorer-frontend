7ef9da9d8428f1ac3114a215659118a862502ec7
Koshin Valery <koshinva1993@gmail.com>
2022-12-27 16:30:28 +0300

    feat: подготовить необходимые роуты

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,26 @@
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Layout from '../Layout';
 import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import Register from '../Register/Register';
+import Login from '../Login/Login';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Main />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Main />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="saved-movies" element={<SavedMovies />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="signup" element={<Register />} />
+        <Route path="signin" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Outlet />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
