805ca34bf818c91644496660edd0f0fab40ba4d0
Koshin Valery <koshinva1993@gmail.com>
2022-12-31 13:24:12 +0300

    feat: сверстать и сделать адаптивную верстку компонента  Profile

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 
 function Layout() {
+  const { pathname } = useLocation();
   return (
     <>
       <Header />
       <div className="container">
         <Outlet />
       </div>
-      <Footer />
+      {pathname !== '/profile' && <Footer />}
     </>
   );
 }
