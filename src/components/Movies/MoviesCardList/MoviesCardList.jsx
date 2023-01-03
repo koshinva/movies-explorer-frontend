c222a798f0565b0bc6c3a40a2a6ef3eceaf6d4dc
Koshin Valery <koshinva1993@gmail.com>
2023-01-03 14:42:54 +0300

    feat: настроить критический случай, когда фильмов нет

--- src/components/Movies/MoviesCardList/MoviesCardList.jsx
+++ src/components/Movies/MoviesCardList/MoviesCardList.jsx
@@ -7,14 +7,20 @@ function MoviesCardList() {
   return (
     <div className="movies-list">
       <div className="movies-list__body">
-        <ul className="movies-list__list">
-          {moviesData.map((movie, i) => (
-            <li key={i} className="movies-list__item movies-list__item_location_movies">
-              <MoviesCard {...movie} />
-            </li>
-          ))}
-        </ul>
-        <button className="movies-list__button-more">Ещё</button>
+        {moviesData.length === 0 ? (
+          <h3 className="movies-list__title-not-movies">Фильмы не найдены</h3>
+        ) : (
+          <>
+            <ul className="movies-list__list">
+              {moviesData.map((movie, i) => (
+                <li key={i} className="movies-list__item movies-list__item_location_movies">
+                  <MoviesCard {...movie} />
+                </li>
+              ))}
+            </ul>
+            <button className="movies-list__button-more">Ещё</button>
+          </>
+        )}
       </div>
     </div>
   );
--- src/components/SavedMovies/MoviesCardList/MoviesCardList.jsx
+++ src/components/SavedMovies/MoviesCardList/MoviesCardList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { moviesData } from '../../../utils/moviesData';
 import MoviesCard from '../MoviesCard/MoviesCard';
 import '../../Movies/MoviesCardList/MoviesCardList.css';
@@ -8,18 +8,22 @@ function MoviesCardList() {
   return (
     <div className="movies-list movies-saved-list">
       <div className="movies-list__body">
-        <ul className="movies-list__list">
-          {moviesData
-            .filter((movie) => movie.liked)
-            .map((movie, i) => (
-              <li key={i} className="movies-list__item">
-                <MoviesCard {...movie} />
-              </li>
-            ))}
-        </ul>
+        {moviesData.length === 0 ? (
+          <h3 className="movies-list__title-not-movies">Сохранённых фильмов нет</h3>
+        ) : (
+          <ul className="movies-list__list">
+            {moviesData
+              .filter((movie) => movie.liked)
+              .map((movie, i) => (
+                <li key={i} className="movies-list__item">
+                  <MoviesCard {...movie} />
+                </li>
+              ))}
+          </ul>
+        )}
       </div>
     </div>
   );
 }
 
-export default MoviesCardList
+export default MoviesCardList;
