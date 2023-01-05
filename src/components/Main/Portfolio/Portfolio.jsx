bd4dad9c03ceb1dd8cec358b6a5d5cae2b28ff9f
Koshin Valery <koshinva1993@gmail.com>
2023-01-05 14:35:35 +0300

    fix: исправить ссылки в компоненте Portfolio

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -7,29 +7,38 @@ function Portfolio() {
       <h3 className="portfolio__title">Портфолио</h3>
       <ul className="portfolio__list-works">
         <li className="portfolio__work">
-          <p className="portfolio__work-name">Статичный сайт</p>
           <a
             href="https://github.com/koshinva/how-to-learn"
             target="_blank"
             rel="noreferrer"
             className="portfolio__work-link"
           >
-            ↗
+            <p className="portfolio__work-name">Статичный сайт</p>
+            <span className="portfolio__work-icon">↗</span>
           </a>
         </li>
         <li className="portfolio__work">
-          <p className="portfolio__work-name">Адаптивный сайт</p>
           <a
             href="https://github.com/koshinva/russian-travel"
             target="_blank"
             rel="noreferrer"
             className="portfolio__work-link"
           >
-            ↗
+            <p className="portfolio__work-name">Адаптивный сайт</p>
+            <span className="portfolio__work-icon">↗</span>
           </a>
         </li>
         <li className="portfolio__work">
-          <p className="portfolio__work-name">Одностраничное приложение</p>
+          <a
+            href="https://github.com/koshinva/react-mesto-api-full"
+            target="_blank"
+            rel="noreferrer"
+            className="portfolio__work-link"
+          >
+            <p className="portfolio__work-name">Одностраничное приложение</p>
+            <span className="portfolio__work-icon">↗</span>
+          </a>
+          {/* <p className="portfolio__work-name">Одностраничное приложение</p>
           <a
             href="https://github.com/koshinva/react-mesto-api-full"
             target="_blank"
@@ -37,7 +46,7 @@ function Portfolio() {
             className="portfolio__work-link"
           >
             ↗
-          </a>
+          </a> */}
         </li>
       </ul>
     </div>
