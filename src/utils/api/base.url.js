3eb0beddbfea90a2c0798412c7722c624c5b93e1
Koshin Valery <koshinva1993@gmail.com>
2023-04-25 15:50:32 +0300

    feat: изменить base_url для vercel

--- src/utils/api/base.url.js
+++ src/utils/api/base.url.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'https://api.movies.koshinva.nomoredomains.club';
+// export const BASE_URL = 'https://api.movies.koshinva.nomoredomains.club';
+export const BASE_URL = 'https://movies-explorer-api-koshinva.vercel.app/';
